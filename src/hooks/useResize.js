9efe13b660e09350076501af46009d3693711a71
Pirskiy Maxim <pirskiymi98@gmail.com>
2023-05-22 13:24:39 +0300

    add useResize hook

---
+++ src/hooks/useResize.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export const SCREEN_SM = 576;
+
+export const useResize = () => {
+   const [width, setWidth] = useState(window.innerWidth);
+
+   useEffect(() => {
+      const handleResize = (event) => {
+         setWidth(event.target.innerWidth);
+      };
+      window.addEventListener('resize', handleResize);
+      return () => {
+         window.removeEventListener('resize', handleResize);
+      };
+   }, []);
+
+   return {
+      width,
+      isScreenSm: width >= SCREEN_SM,
+   };
+};
